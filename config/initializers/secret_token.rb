d9ae081a44492761c8473be7a6e50abb76f4ab59
Miguel Palhas <mpalhas@gmail.com>
2012-10-23 11:08:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faqu::Application.config.secret_token = '90973438b1f33855022205ccbe31ef150b076eabc584c92630056263ddf1a894c2778698ed72848314431e7ebaa2df0bf59e8ab22fcfc67eb017ea1601cd100c'
